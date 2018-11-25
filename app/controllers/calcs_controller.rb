4a4066242120cdbf2ddb9eef9b479b2b59aa6470
kamatanikyouhei <kamatanikyouhei@kamatanikyouheinoMacBook-ea.local>
2018-11-25 22:45:16 +0900

    課題修正1

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,29 +1,14 @@
 class CalcsController < ApplicationController
   def show
     @msg1 = params[:msg1].to_i 
-    @addition = "addition"
+    @msg2 = params["addition"]
     @msg3 = params[:msg3].to_i
     @msg4 = @msg1 + @msg3
+    if @msg2 = params["subtraction"]
+      @msg4 = @msg1 - @msg3 ;
+    elsif @msg2 = params["multiplication"]
+      @msg4 = @msg1 * @msg3 ;
+    else  @msg2 = params["division"]
+      @msg4 = @msg1 / @msg3
+    end
   end
-
-  def show2
-    @msg1 = params[:msg1].to_i 
-    @subtraction = "subtraction"
-    @msg3 = params[:msg3].to_i
-    @msg5 = @msg1 - @msg3
-  end
-
-  def show3
-    @msg1 = params[:msg1].to_i 
-    @multiplication = "multiplication"
-    @msg3 = params[:msg3].to_i
-    @msg6 = @msg1 * @msg3
-  end
-
-  def show4
-    @msg1 = params[:msg1].to_i 
-    @division =  "division"
-    @msg3 = params[:msg3].to_i
-    @msg7 = @msg1 / @msg3
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "calcs/:msg1/addition/:msg3" => "calcs#show"
-  get "calcs/:msg1/subtraction/:msg3" => "calcs#show2"
-  get "calcs/:msg1/multiplication/:msg3" => "calcs#show3"
-  get "calcs/:msg1/division/:msg3" => "calcs#show4"
+  get "calcs/:msg1/:msg2/:msg3" => "calcs#show"
 end
